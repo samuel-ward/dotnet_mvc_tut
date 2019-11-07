48b52510b3418772f88541945b9f27a1ed5d7b7c
Sam Ward <sam.ward@ripetime.com>
2019-11-08 12:03:48 +1300

    Database Implementation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotnet_mvc_tut.Models
 {
@@ -8,9 +9,12 @@ namespace dotnet_mvc_tut.Models
         public int Id {get;set;}
         public string Title {get;set;}
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate {get;set;}
         public string Genre {get;set;}
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price {get;set;}
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using dotnet_mvc_tut.Data;
+using dotnet_mvc_tut.Models;
 
 namespace dotnet_mvc_tut
 {
@@ -13,7 +16,24 @@ namespace dotnet_mvc_tut
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An wrror occurred seeding the DB");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
